f194d30a4d1a74e911e3350f9a8962b3cadf997f
khu-kuki1504 <kuki1504@khu.ac.kr>
2022-03-25 14:32:28 +0900

    add GoBeegoProject (To be Changed Gin -> Beego)

---
+++ src/GoBeegoProject/controllers/api/v1/user.go
@@ -0,0 +1,46 @@
+package v1
+
+import (
+	u "GoBeegoProject/apiHelpers"
+	v1s "GoBeegoProject/services/api/v1"
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func TestUserList(c *gin.Context) {
+	var userService v1s.UserService
+
+	//decode the request body into struct and failed if any error occur
+	err := json.NewDecoder(c.Request.Body).Decode(&userService.User)
+	if err != nil {
+		u.Respond(c.Writer, u.Message(1, "Invalid request"))
+		return
+	}
+
+	//call service
+	resp := userService.UserList()
+
+	//return response using api helper
+	u.Respond(c.Writer, resp)
+
+}
+
+//UserList function will give you the list of users
+func UserList(c *gin.Context) {
+	var userService v1s.UserService
+
+	//decode the request body into struct and failed if any error occur
+	err := json.NewDecoder(c.Request.Body).Decode(&userService.User)
+	if err != nil {
+		fmt.Println(err)
+		u.Respond(c.Writer, u.Message(1, "Invalid request"))
+		return
+	}
+
+	//call service
+	resp := userService.UserList()
+
+	//return response using api helper
+	u.Respond(c.Writer, resp)
+}
